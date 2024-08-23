a48e87bce9b93062f578439c4b22a2483d978336
Lyonns <tashielyonns@gmail.com>
2024-08-23 14:39:38 -0400

    Changed directory

--- spindys-tournament-1/vite.config.js
+++ spindys-tournament-1/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './src',
+  root: '.', // Remove this line or set to '.' if you want to explicitly specify the root
 });
